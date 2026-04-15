5d495a4605a0fc12065d90767c4d1a3cfaee3e9e
dfamrln <mhdffa1@gmail.com>
2026-04-15 07:49:46 +0700

    Add project files.

---
+++ sql/SQLQuery1.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE UkiranKayuDB;
+GO
+
+USE UkiranKayuDB;
+GO
+
+CREATE TABLE Produk (
+    id_produk INT IDENTITY(1,1) PRIMARY KEY,
+    nama_produk VARCHAR(100) NOT NULL,
+    jenis_kayu VARCHAR(50) NOT NULL,
+    harga INT NOT NULL,
+    stok INT NOT NULL
+);
+
+CREATE TABLE Pesanan (
+    id_pesanan INT IDENTITY(1,1) PRIMARY KEY,
+    tanggal_pesanan DATE NOT NULL,
+    status VARCHAR(50),
+    total_harga INT
+);
+
+CREATE TABLE DetailPesanan (
+    id_detail INT IDENTITY(1,1) PRIMARY KEY,
+    id_pesanan INT,
+    id_produk INT,
+    jumlah INT,
+    subtotal INT,
+
+    FOREIGN KEY (id_pesanan) REFERENCES Pesanan(id_pesanan),
+    FOREIGN KEY (id_produk) REFERENCES Produk(id_produk)
+);
+
+INSERT INTO Produk (nama_produk, jenis_kayu, harga, stok)
+VALUES 
+('Meja Ukir Jepara','Jati',1500000,10),
+('Kursi Ukir','Mahoni',800000,20),
+('Lemari Ukir','Jati',2500000,5);
+
